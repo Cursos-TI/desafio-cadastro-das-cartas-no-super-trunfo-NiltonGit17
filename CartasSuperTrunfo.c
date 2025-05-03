77f27016eeeb2e34b70308da3b9493916082f2e3
Nilton Vaz <ads17vaz@gmail.com>
2025-05-03 01:02:09 +0000

    novo commit st

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    
     // Declaração das variáveis da primeira carta
-    char estado1;
-    char codigo1[4];
-    char nomeCidade1[50];
     int populacao1;
     float area1;
     float pib1;
     int pontosTuristicos1;
 
     // Declaração das variáveis da segunda carta
-    char estado2;
-    char codigo2[4];
-    char nomeCidade2[50];
     int populacao2;
     float area2;
     float pib2;
@@ -22,18 +17,9 @@ int main() {
     // Cadastro da Carta 1
     printf("1:\n");
 
-    printf("A a H: ");
-    scanf(" %c", &estado1);
-
-    printf("17: ");
-    scanf("%s", codigo1);
-
-    printf("Nome da Cidade : ");
-    scanf("%s", nomeCidade1);
-
     printf("População: ");
     scanf("%d", &populacao1);
-
+    
     printf("Área (em km²): ");
     scanf("%f", &area1);
 
@@ -45,15 +31,6 @@ int main() {
 
     // Cadastro da Carta 2
     printf("2:\n");
-
-    printf(" A a H): ");
-    scanf(" %c", &estado2);
-
-    printf(" 18: ");
-    scanf("%s", codigo2);
-
-    printf("Nome da Cidade: ");
-    scanf("%s", nomeCidade2);
 
     printf("População: ");
     scanf("%d", &populacao2);
@@ -71,18 +48,12 @@ int main() {
     printf("\n=== Cartas Cadastradas ===\n");
 
     printf("Carta 1:\n");
-    printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", nomeCidade1);
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
 
     printf("\nCarta 2:\n");
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", nomeCidade2);
     printf("População: %d\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
